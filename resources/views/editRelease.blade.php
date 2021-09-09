0998dfa1091ddbee3c2682bca46f585e0dbc5941
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2021-09-09 09:27:46 -0400

    css for edit page sorted out

--- resources/views/editRelease.blade.php
+++ resources/views/editRelease.blade.php
@@ -1,7 +1,35 @@
 <x-navbar></x-navbar>
-<form method="post" action="{{route('api.release.edit', ['id'=>$release->id])}}">
-    <input type="text" id="artist" value="{{$release->artist}}">
-    <input type="text" id="title" value="{{$release->title}}">
-    <input type="number" id="shelf_order" value="{{$release->shelf_order}}">
-    <input type="submit">
-</form>
+<div class="">
+    <form class="col-lg-6 offset-lg-3 " method="post" action="{{route('api.release.edit', ['id'=>$release->id])}}">
+        <div class="row justify-content-center">
+            <div class="col-auto">
+                <label>Cover Image</label>
+                <br>
+                <img src="{{$release->thumbnail}}">
+                <br>
+                <btn class="btn btn-primary mt-2">Change Cover Image</btn>
+            </div>
+            <div class="col-5">
+                <div class="form-group">
+                    <label for="artist">Artist</label>
+                    <br>
+                    <input type="text" class="form-control" id="artist" value="{{$release->artist}}">
+                </div>
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <br>
+                    <input type="text" class="form-control" id="title" value="{{$release->title}}">
+                </div>
+                <div class="form-group">
+                    <label for="shelf_order">Shelf Position</label>
+                    <br>
+                    <input type="number" class="form-control-sm" id="shelf_order" min="1"
+                           max="{{count(\App\Models\Release::all())}}" value="{{$release->shelf_order}}">
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <input type="submit">
+        </div>
+    </form>
+</div>
