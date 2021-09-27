05b17ecc983f4339a862d21c5a63f64e9b24b50f
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2021-09-27 15:17:25 -0400

    can update shelf_order properly

--- app/Http/Controllers/Release/Update/Update.php
+++ app/Http/Controllers/Release/Update/Update.php
@@ -10,13 +10,23 @@ class Update extends Controller
 {
     public function __invoke(Request $request, $id)
     {
-        foreach ($request->all() as $key => $value){
-            if (!$value){
+        foreach ($request->all() as $key => $value) {
+            if (!$value) {
                 $request->request->remove($key);
             }
         }
         $release = Release::query()->where('id', $id)->first();
+        $originalPosition = $release->shelf_order;
+        if ($request->input('shelf_order') != $originalPosition) {
+            $this->changeShelfOrder($originalPosition, $request->input('shelf_order'));
+        }
         $release->update($request->all());
         return redirect(route('release.edit.show', $release->id));
+    }
+
+    private function changeShelfOrder($originalPosition, $newPosition)
+    {
+        $releasesToLowerInOrder = Release::query()->whereBetween('shelf_order', [$originalPosition + 1, $newPosition])->get();
+        $releasesToLowerInOrder->each(fn($release) => $release->update(['shelf_order' => (int)$release->shelf_order - 1]));
     }
 }
--- tests/Http/Controllers/Release/Update/UpdateTest.php
+++ tests/Http/Controllers/Release/Update/UpdateTest.php
@@ -12,26 +12,40 @@ class UpdateTest extends TestCase
     use withFaker;
     use RefreshDatabase;
 
-    public function testItCanReturnASuccess()
+    public function testItCanReturnARedirect()
     {
         $release = Release::factory()->create();
-        $this->post(route('api.release.edit', $release->id))->assertSuccessful();
+        $this->post(route('api.release.edit', $release->id))->assertRedirect();
     }
 
     public function testItCanUpdateARelease()
     {
-        $this->withoutExceptionHandling();
         $release = Release::factory()->create();
         $newArtist = $this->faker->name;
         $newTitle = $this->faker->sentence;
         $this->postJson(route('api.release.edit', $release->id), [
             'artist' => $newArtist,
             'title' => $newTitle,
-        ])->assertSuccessful();
+        ]);
         $this->assertDatabaseHas('releases', [
             'id' => $release->id,
             'artist' => $newArtist,
             'title' => $newTitle,
         ]);
+
+    }
+
+    public function testItCanChangeShelfOrderCorrectly()
+    {
+        $releases = Release::factory()->count(10)->create();
+        $releaseToChange = $releases[3];
+        dump($releaseToChange->toArray());
+        $this->postJson(route('api.release.edit', $releaseToChange->id), [
+            'shelf_order' => '7',
+        ]);
+        $reorderedReleases = Release::all()->sortByDesc('shelf_order');
+        dump($releaseToChange->fresh()->toArray());
+        dump($reorderedReleases[3]->toArray());
+        $this->assertEquals($releaseToChange->fresh()->toArray(), $reorderedReleases[3]->toArray());
     }
 }
