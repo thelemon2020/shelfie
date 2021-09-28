346f7145fab2b3174da81bc1a78b30130bf80146
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2021-09-28 15:40:08 -0400

    WIP - reset index page on filter change

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -4,16 +4,35 @@ namespace App\Http\Livewire;
 
 use App\Models\Release;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Search extends Component
 {
+    use withPagination;
+
+    protected $paginationTheme = 'bootstrap';
+
     public $search = '';
-    public $sort ='artist';
+    public $sort = 'artist';
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
 
     public function render()
     {
+        if ($this->sort == 'genre') {
+            $releases = Release::query()
+                ->join('genres', 'releases.genre_id', '=', 'genres.id')
+                ->where('genres.name', "LIKE", "%$this->search%")
+                ->orderBy('genres.name', 'ASC')
+                ->paginate();
+        } else {
+            $releases = Release::query()->where($this->sort, "LIKE", "%$this->search%")->orderBy($this->sort, 'ASC')->paginate();
+        }
         return view('livewire.search', [
-            'releases' => Release::query()->where($this->sort, "LIKE", "%$this->search%")->orderBy($this->sort, 'ASC')->paginate()
+            'releases' => $releases
         ]);
     }
 }
