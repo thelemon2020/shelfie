df572e8e76ee36087c7f513f52db28a8e1ce8bd3
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2021-10-15 09:26:16 -0400

    Implements adding genres on edit and create pages

--- app/Http/Livewire/Edit.php
+++ app/Http/Livewire/Edit.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Genre;
 use App\Models\Release;
 use Carbon\Carbon;
 use Exception;
@@ -10,11 +11,30 @@ use Livewire\Component;
 
 class Edit extends Component
 {
-    public $release;
+    protected $release;
+    public $artist;
+    public $title;
+    public $release_year;
+    public $shelf_order;
+    public $full_image;
+    public $thumbnail;
+    public $allGenres;
+    public $genre;
+
+    protected function getListeners()
+    {
+        return ['genreAdded' => 'newGenre'];
+    }
+
+    public function mount($release = null)
+    {
+        $this->release = $release;
+        $this->allGenres = Genre::all();
+    }
 
     public function render()
     {
-        return view('livewire.edit');
+        return view('livewire.edit', ['release' => $this->release]);
     }
 
     public function updated($propertyName)
@@ -22,28 +42,55 @@ class Edit extends Component
         $this->validateOnly($propertyName);
     }
 
-    public function submit()
+    public function updatedGenre()
     {
-        $this->validate();
-        try {
-            dd('trying to save ', $this->release);
-            $this->release->save();
-        } catch (Exception $e) {
-            session()->flash('error', 'Release could not be updated.');
+        if ($this->genre === "add-modal") {
+            $this->dispatchBrowserEvent('add-genre');
         }
 
+    }
+
+    public function submit()
+    {
+        if ($this->release === null) {
+            $this->release = Release::query()->make();
+        }
+        $this->validate();
+        try {
+            $this->release->updateOrCreate([
+                'artist' => $this->artist,
+                'title' => $this->title,
+                'release_year' => $this->release_year,
+            ],
+                [
+                    'thumbnail' => $this->thumbnail,
+                    'full_image' => $this->full_image,
+                    'shelf_order' => $this->shelf_order,
+                    'genre_id' => Genre::query()->where('name', $this->genre)->first()->id
+                ]);
+        } catch (Exception $e) {
+            session()->flash('error', 'Release could not be updated.');
+            return false;
+        }
         session()->flash('message', 'Release successfully updated.');
+    }
+
+    public function newGenre()
+    {
+        $this->allGenres = Genre::all();
+        $this->render();
     }
 
     protected function rules()
     {
         return [
-            'release.artist' => 'required',
-            'release.title' => 'required',
-            'release.release_year' => ['required', 'integer', 'max:' . Carbon::now()->year],
-            'release.shelf_order' => ['required', 'integer', 'max:' . Release::query()->count() + 1],
-            'release.thumbnail' => ['required', 'url'],
-            'release.full_image' => ['required', 'url'],
+            'artist' => 'required',
+            'title' => 'required',
+            'release_year' => ['required', 'integer', 'max:' . Carbon::now()->year],
+            'shelf_order' => ['required', 'integer', 'max:' . Release::query()->count() + 1],
+            'thumbnail' => ['required', 'url'],
+            'full_image' => ['required', 'url'],
+            'genre' => 'required'
         ];
     }
 }
---
+++ resources/views/livewire/genres.blade.php
@@ -0,0 +1,20 @@
+<div>
+    <form wire:submit.prevent="submit">
+        <div class="form-group">
+            <label for="genre">Add a Genre</label>
+            <br>
+            <input type="text" class="form-control" name="genre" id="genre" wire:model="genre">
+            @error('genre') <span class="error">{{ $message }}</span> @enderror
+        </div>
+        <div class="modal-footer">
+            <div class="mr-auto">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            <div class="align-self-end">
+                <button type="button" wire:click="submit" data-dismiss="modal" class="btn btn-primary">
+                    Add
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
