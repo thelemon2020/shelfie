e0085116e200a13ff40b1a4cba905db1ae545fdd
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2021-11-15 16:01:20 -0500

    add user settings table to aid in setting up lights

---
+++ database/migrations/2021_11_15_125828_create_user_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_settings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('sort_method');
+            $table->string('wled_ip');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_settings');
+    }
+}
