4c4750e63ebef813bc4424d98589b6c582577f61
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2021-11-16 08:02:22 -0500

    set up accordion on collection manage page

--- resources/views/manageCollection.blade.php
+++ resources/views/manageCollection.blade.php
@@ -10,6 +10,40 @@
 <div>
     <a class="btn btn-primary m-2" href="{{route('collection.index')}}">Return to Collection</a>
 </div>
+<div id="accordion">
+    <div class="card">
+        <div class="card-header" id="headingOne">
+            <h5 class="mb-0">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false"
+                        aria-controls="collapseOne">
+                    User Settings
+                </button>
+            </h5>
+        </div>
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false"
+                        aria-controls="collapseOne">
+                    LED Settings
+                </button>
+            </h5>
+        </div>
+        <div id="collapseTwo" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+                @if(\App\Models\User::all()->first()->userSettings->sort_method == 'genre')
+                    <livewire:manage-genres/>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
-<livewire:manage-genres/>
+
 @livewireScripts
