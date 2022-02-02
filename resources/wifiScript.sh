e12bfe66d5f2013a0f484354d2f98db784faed82
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-02-02 17:57:51 -0500

    fix multiline string issue with bash

--- resources/wifiScript.sh
+++ resources/wifiScript.sh
@@ -2,15 +2,15 @@
 
 DESTDIR=/etc/wpa_supplicant/wpa_supplicant.conf
 
-OUTPUT="country=us
-       update_config=1
-       ctrl_interface=/var/run/wpa_supplicant
+OUTPUT="country=us\n\
+       update_config=1\n\
+       ctrl_interface=/var/run/wpa_supplicant\n\
+        \n\
+       network={\n\
+        scan_ssid=1\n\
+        ssid=\"$1\"\n\
+        psk=\"$2\"\n\
+       }"
 
-       network={
-        scan_ssid=1
-        ssid=\"$1\"
-        psk=\"$2\"
-       }\n"
-
-echo -e "$3\n" | sudo -S sh -c "echo $OUTPUT > $DESTDIR"
+echo -e "$3\n" | sudo -S sh -c "echo -e $OUTPUT > $DESTDIR"
 
