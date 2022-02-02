a5d577a58deae7fefc59740c3591e67fa40df513
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-02-02 14:38:34 -0500

    try to run the script directly

--- app/Http/Livewire/Wifi.php
+++ app/Http/Livewire/Wifi.php
@@ -3,10 +3,7 @@
 namespace App\Http\Livewire;
 
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Livewire\Component;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
 
 class Wifi extends Component
 {
@@ -21,12 +18,13 @@ class Wifi extends Component
     public function submit()
     {
         $this->validate();
-        $wifiScript = new Process(['sudo', '/var/www/html/shelfie/resources/wifiScript.sh', $this->ssid, $this->password, config('auth.rp_password')]);
-        $wifiScript->run();
-        if (!$wifiScript->isSuccessful()) {
-            Log::error($wifiScript->getErrorOutput());
+        $runstatus = exec('sudo /var/www/html/shelfie/resources/wifiScript.sh ' . $this->ssid . ' ' . $this->password . ' ' . config('auth.rp_password'));
+//        $wifiScript = new Process(['sudo', '/var/www/html/shelfie/resources/wifiScript.sh', $this->ssid, $this->password, config('auth.rp_password')]);
+//        $wifiScript->run();
+        if (!$runstatus->isSuccessful()) {
+            //Log::error($wifiScript->getErrorOutput());
             $this->addError('connection', 'Internal Server Error');
-            throw new ProcessFailedException($wifiScript);
+            //throw new ProcessFailedException($wifiScript);
         } else {
             $response = Http::timeout(10)->get('https://google.com');
             if ($response->status() !== 200) {
