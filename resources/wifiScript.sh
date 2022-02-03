682a0e38126cf40c88957abb69f1b69edaf5cec5
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-02-02 20:02:17 -0500

    tweaks to the script

--- resources/wifiScript.sh
+++ resources/wifiScript.sh
@@ -2,15 +2,6 @@
 
 DESTDIR=/etc/wpa_supplicant/wpa_supplicant.conf
 
-OUTPUT="country=us\n\
-       update_config=1\n\
-       ctrl_interface=/var/run/wpa_supplicant\n\
-        \n\
-       network={\n\
-        scan_ssid=1\n\
-        ssid=\"$1\"\n\
-        psk=\"$2\"\n\
-       }"
+OUTPUT="country=us\n\update_config=1\n\ctrl_interface=/var/run/wpa_supplicant\n\\n\network={\n\scan_ssid=1\n\ssid=\"$1\"\n\psk=\"$2\"\n\}"
 
 echo -e "$3\n" | sudo -S sh -c "echo -e \"$OUTPUT\" > \"$DESTDIR\""
-
