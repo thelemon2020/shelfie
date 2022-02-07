2d822201c54d5256fd75f176ce8ebea8b2d56f6d
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-02-07 09:17:59 -0500

    remove some superfluous code

--- app/Http/Livewire/Wifi.php
+++ app/Http/Livewire/Wifi.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Livewire;
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 use Symfony\Component\Process\Exception\ProcessFailedException;
@@ -23,17 +22,10 @@ class Wifi extends Component
         $this->validate();
         $wifiScript = new Process(['/var/www/html/shelfie/resources/wifiScript.sh', $this->ssid, $this->password, config('auth.rp_password')]);
         $wifiScript->run();
-        if (!$wifiScript->isSuccessful()) {
+        if ($wifiScript->getStatus() !== '255') {
             Log::error($wifiScript->getErrorOutput());
             $this->addError('connection', 'Internal Server Error');
             throw new ProcessFailedException($wifiScript);
-        } else {
-            $response = Http::timeout(10)->get('https://google.com');
-            if ($response->status() !== 200) {
-                $this->addError('connection', 'Could Not Connect To Internet');
-            } else {
-                session()->flash('message', 'Connection Successful');
-            }
         }
 
         $this->render();
