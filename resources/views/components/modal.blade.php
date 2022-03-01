fec2df7c3c9519cb7191110bb297b3b5cd37acef
Vehikl Team <nomorobo-vehikl@gmail.com>
2022-03-01 16:50:26 -0500

    update modal to use tailwind styling

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,46 +1,46 @@
-
-<div class="absolute top-0 left-0 bg-gray-500/50 h-full w-full flex" id="detailsModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+<div class="absolute top-0 left-0 bg-gray-500/50 h-full w-full flex" id="detailsModal" tabindex="-1" role="dialog"
+     aria-labelledby="exampleModalCenterTitle"
      aria-hidden="true">
-    <div class="mx-auto my-auto bg-white w-1/3" role="document">
+    <div class="mx-auto my-auto bg-white w-auto" role="document">
         <div class="h-100 p-2 shadow-lg">
-            <div class="modal-header flex-shrink-0">
-                <h5 class="modal-title" id="modal-title">Record Details</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+            <div class="relative border-b border-black">
+                <h5 class="text-lg" id="modal-title">Record Details</h5>
+                <button type="button" class="absolute top-0 right-0" data-bs-dismiss="modal" aria-label="Close">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                         stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>
+                    </svg>
                 </button>
             </div>
-            <div class="modal-body d-flex flex-column flex-grow-1">
-                <div class="container flex-grow-1">
-                    <div class="row">
-                        <div class="col align-self-center">
-                            <img id="thumbnail" class="w-100 h-100" src="">
-                        </div>
-                        <div class="col-4">
-                            <label><b>Artist</b></label>
-                            <p id="artist"></p>
-                            <label><b>Title</b></label>
-                            <p id="title"></p>
-                            <label><b>Genre</b></label>
-                            <p id="genre"></p>
-                            <p>Played <span id="timesPlayed" ></span> times</p>
-                            <p>Last played on <span id="lastPlayedAt"></span></p>
-                            <input id="releaseId" hidden value="">
-                        </div>
-                    </div>
+            <div class="flex justify-around items-center py-2">
+                <img id="thumbnail" class="p-1 w-3/4"
+                     src="https://img.discogs.com/D4sQeozP2N0syrTXlmPL8Z-AHfg=/fit-in/600x606/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-3155658-1402218436-4768.jpeg.jpg">
+                <div class="p-1 w-1/4">
+                    <label><b>Artist</b></label>
+                    <p id="artist" class="mb-2">Test</p>
+                    <label><b>Title</b></label>
+                    <p id="title" class="mb-2">sjfkashf</p>
+                    <label><b>Genre</b></label>
+                    <p id="genre" class="mb-2">afhbsajfnjak</p>
+                    <p class="mb-2">Played <span id="timesPlayed">11</span> times</p>
+                    <p class="mb-2">Last played on<br><span id="lastPlayedAt">2022-08-24</span></p>
+                    <input id="releaseId" hidden value="">
                 </div>
-                <div class="modal-footer p-0">
-                    <div class="mr-auto">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                            Close
-                        </button>
-                        <a class="btn btn-secondary" id="edit" onclick="turnOffLight()" href="">Edit</a>
-                        <button type="button" class="btn btn-danger" onclick="deleteRecord()">Delete</button>
-                    </div>
-                    <div class="align-self-end">
-                        <button type="button" onclick="playRecord()" data-bs-dismiss="modal" class="btn btn-primary">
-                            Play
-                            Album
-                        </button>
-                    </div>
+            </div>
+            <div class="flex border-t border-black justify-between pt-2">
+                <div class="">
+                    <button type="button" class="p-4 bg-blue-500 rounded-lg" data-dismiss="modal">
+                        Close
+                    </button>
+                    <a class="p-5 bg-yellow-500 rounded-lg" id="edit" onclick="turnOffLight()" href="">Edit</a>
+                    <button type="button" class="p-4 bg-red-500 rounded-lg" onclick="deleteRecord()">Delete</button>
+                </div>
+                <div class="">
+                    <button type="button" onclick="playRecord()" data-bs-dismiss="modal"
+                            class="p-4 bg-blue-500 rounded-lg">
+                        Play
+                        Album
+                    </button>
                 </div>
             </div>
         </div>
