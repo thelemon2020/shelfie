aa7aa7b611483264e09aebc6c1ba59c80126395f
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-04-26 10:05:58 -0400

    replace bootstrap with tailwind on all elements

--- resources/views/components/stats-comp.blade.php
+++ resources/views/components/stats-comp.blade.php
@@ -1,57 +1,49 @@
 <script src="https://unpkg.com/echarts/dist/echarts.min.js"></script>
 <!-- Chartisan -->
 <script src="https://unpkg.com/@chartisan/echarts/dist/chartisan_echarts.js"></script>
-<div id="statsCarousel" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item vh-100">
-            <div class="text-center">
-                <h3>Most Played Records</h3>
-                <table class="table">
-                    <thead>
-                    <th style="width: 15vh"></th>
-                    <th>Artist</th>
-                    <th>Title</th>
-                    <th>Times Played</th>
-                    </thead>
-                    <tbody>
-                    @foreach($mostPlayed as $release)
-                        <tr>
-                            <td><img class="img-fluid" src="{{$release->full_image ?? ''}}">
-                            </td>
-                            <td>{{$release->artist ?? ''}}</td>
-                            <td>{{$release->title ?? ''}}</td>
-                            <td>{{$release->times_played ?? ''}}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div class="text-center">
-                <h3 class="mt-2 mb-0">Last Played</h3>
-                @if(!$lastPlayed)
-                    <h4>Nothing! Go Spin A Record!</h4>
-                @else
-                    <img id="lastPlayed" class="rounded mx-auto" src="{{$lastPlayed->full_image  ?? ''}}">
-                    <h4>{{$lastPlayed->artist ?? ''}} - {{$lastPlayed->title ?? ''}}</h4>
-                    <h4>Last Played
-                        At: {{\Carbon\Carbon::parse($lastPlayed->last_played_at)->setTimezone('America/Toronto')->format('g:i a d/m/y') ?? ''}}</h4>
-                @endif
-            </div>
-        </div>
-        <div class="carousel-item">
-            <div id="chart-days" style="height: 50vh;"></div>
-        </div>
-        <div class="carousel-item">
-            <div id="chart-genre" style="height: 50vh;"></div>
-        </div>
-        <div class="carousel-item active">
-            <div id="chart-artists" style="height: 50vh;"></div>
-        </div>
+<div id="statsCarousel" class="min-w-screen text-center flex items-center justify-center">
+    <div id="mostPlayedStat" class="c">
+        <h3>Most Played Records</h3>
+        <table>
+            <thead>
+            <th style="width: 15vh"></th>
+            <th class="w-4/12">Artist</th>
+            <th>Title</th>
+            <th>Times Played</th>
+            </thead>
+            <tbody>
+            @foreach($mostPlayed as $release)
+                <tr>
+                    <td><img class="img-fluid" src="{{$release->full_image ?? ''}}"></td>
+                    <td>{{$release->artist ?? ''}}</td>
+                    <td>{{$release->title ?? ''}}</td>
+                    <td>{{$release->times_played ?? ''}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
+    <div class="hidden" id="lastPlayedStat">
+        <h3>Last Played</h3>
+        @if(!$lastPlayed)
+            <h4>Nothing! Go Spin A Record!</h4>
+        @else
+            <img id="lastPlayed" class="rounded mx-auto" src="{{$lastPlayed->full_image  ?? ''}}">
+            <h4>{{$lastPlayed->artist ?? ''}} - {{$lastPlayed->title ?? ''}}</h4>
+            <h4>Last Played
+                At: {{\Carbon\Carbon::parse($lastPlayed->last_played_at)->setTimezone('America/Toronto')->format('g:i a d/m/y') ?? ''}}</h4>
+        @endif
+    </div>
+
+    <div id="chart-days" class="hidden w-full h-72"></div>
+
+
+    <div id="chart-genre" class="hidden w-full h-72"></div>
+
+
+    <div id="chart-artists" class="hidden w-full h-[72vh]"></div>
+
 </div>
-<!-- Your application script -->
 <script>
     const chartDays = new Chartisan({
         el: '#chart-days',
