5a03b88b55a2d4bd787aa0f067195d68cca842a7
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-04-29 09:34:35 -0400

    update options page to work better on the raspi

--- resources/views/components/options.blade.php
+++ resources/views/components/options.blade.php
@@ -1,16 +1,63 @@
-<x-accordion name="User Settings">
-    <livewire:user-settings/>
-</x-accordion>
+<ul class="" id="options-pills-tab" role="tablist">
+    <li class="inline-block mr-2"
+        role="presentation">
+        <button class="p-4 raspi:p-1 rounded-lg" onclick="changeOptionTab('#pills-user-settings')"
+                id="pills-user-settings-tab"
+                type="button" role="option-tab" aria-controls="pills-playing"
+                aria-selected="true">User Settings
+        </button>
+    </li>
+    <li class="inline-block mr-2" role="presentation">
+        <button class="p-4 raspi:p-1 bg-blue-500 rounded-lg text-white" onclick="changeOptionTab('#pills-sort-options')"
+                id="pills-sort-options-tab"
+                data-bs-toggle="pill"
+                data-bs-target="#pills-collection"
+                type="button" role="option-tab" aria-controls="pills-collection" aria-selected="false">Sort Options
+        </button>
+    </li>
+    <li class="inline-block mr-2" role="presentation">
+        <button class="p-4 raspi:p-1 bg-blue-500 rounded-lg text-white"
+                onclick="changeOptionTab('#pills-light-segments')" id="pills-light-segments-tab" data-bs-toggle="pill"
+                data-bs-target="#pills-stats"
+                type="button" role="option-tab" aria-controls="pills-stats" aria-selected="false">Manage Light Segments
+        </button>
+    </li>
+    <li class="inline-block mr-2" role="presentation">
+        <button class="p-4 raspi:p-1 bg-blue-500 rounded-lg text-white"
+                onclick="changeOptionTab('#pills-manage-genres')" id="pills-manage-genres-tab" data-bs-toggle="pill"
+                data-bs-target="#pills-options"
+                type="button" role="option-tab" aria-controls="pills-options" aria-selected="false">Manage Genres
+        </button>
+    </li>
+</ul>
+<div class="tab-content" id="pills-tabContent">
+    <div id="pills-user-settings" role="option-panel"
+         class=""
+         aria-labelledby="pills-playing-tab">
+        <livewire:user-settings/>
+    </div>
+    <div class="hidden" id="pills-sort-options" role="option-panel"
+         aria-labelledby="pills-collection-tab">
+        <livewire:sort-options/>
+    </div>
+    <div class="hidden" id="pills-light-segments" role="option-panel"
+         aria-labelledby="pills-stats-tab">
+        <livewire:manage-light-segments/>
 
-<x-accordion name="Sort Options">
-    <livewire:sort-options/>
-</x-accordion>
+    </div>
+    <div class="hidden" id="pills-manage-genres" role="option-panel"
+         aria-labelledby="pills-options-tab">
+        <livewire:manage-genres/>
+    </div>
+</div>
 
-<x-accordion name="Manage Lights">
-    <livewire:manage-light-segments/>
-</x-accordion>
-
-<x-accordion name="Manage Genres">
-    <livewire:manage-genres/>
-</x-accordion>
-
+<script>
+    function changeOptionTab(pageId) {
+        $("[role='option-panel']").addClass('hidden')
+        $("[role='option-tab']").addClass('bg-blue-500')
+        $("[role='option-tab']").addClass('text-white')
+        $(pageId).toggleClass('hidden')
+        $(pageId + '-tab').removeClass('bg-blue-500')
+        $(pageId + '-tab').removeClass('text-white')
+    }
+</script>
--- resources/views/livewire/manage-light-segments.blade.php
+++ resources/views/livewire/manage-light-segments.blade.php
@@ -9,7 +9,8 @@
             @foreach($segments as $segment)
                 <livewire:update-light-segments :segment="$segment" :wire:key="$segment->id"/>
             @endforeach
-            <button type="button" class="bg-blue-500 p-4 m-1 h-72 w-72 rounded rounded-lg text-white"
+            <button type="button"
+                    class="bg-blue-500 p-4 m-1 raspi:h-[65vh] raspi:w-[65vh] h-72 w-72 rounded rounded-lg text-white"
                     wire:click="createSegment">
                 <svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6" fill="none" viewBox="0 0 24 24"
                      stroke="currentColor" stroke-width="2">
@@ -20,6 +21,8 @@
                 Add LED Segment
             </button>
         </div>
-        <button type="submit" class="bg-blue-500 rounded p-4 rounded-md text-lg mb-1 text-white">Update</button>
+        <button type="submit"
+                class="bg-blue-500 rounded p-4 raspi:p-1 rounded-md raspi:text-md text-lg mb-1 text-white">Update
+        </button>
     </form>
 </div>
