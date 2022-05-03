93f9b357af5b689b2d41372f8dcbc15b2aad1ca2
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-05-03 16:37:53 -0400

    update register flow

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,57 +38,49 @@
 
 </script>
 <div id="home" class="overflow-hidden">
-    @if(\App\Models\User::query()->first())
-        <div class="tab-content" id="pills-tabContent">
-            <div id="pills-playing" class="@if(\App\Models\User::query()->count() <1) hidden @endif" role="tabpanel"
-                 aria-labelledby="pills-playing-tab">
-                <div class="text-center">
-                    <h3>Now Playing</h3>
-                    <br>
-                    @if(!$nowPlaying)
-                        <h4>Nothing! Go Spin A Record!</h4>
-                    @else
-                        <img class="raspi:w-48 h-48 inline-flex" id="lastPlayed"
-                             src="{{$nowPlaying->full_image  ?? ''}}">
-                        <h4>{{$nowPlaying->artist ?? ''}} - {{$nowPlaying->title ?? ''}}</h4>
-                    @endif
+    <div class="tab-content" id="pills-tabContent">
+        <div id="pills-playing" class="@if(\App\Models\User::query()->count() <1) hidden @endif" role="tabpanel"
+             aria-labelledby="pills-playing-tab">
+            <div class="text-center">
+                <h3>Now Playing</h3>
+                <br>
+                @if(!$nowPlaying)
+                    <h4>Nothing! Go Spin A Record!</h4>
+                @else
+                    <img class="raspi:w-48 h-48 inline-flex" id="lastPlayed"
+                         src="{{$nowPlaying->full_image  ?? ''}}">
+                    <h4>{{$nowPlaying->artist ?? ''}} - {{$nowPlaying->title ?? ''}}</h4>
+                @endif
+            </div>
+        </div>
+        <div class="hidden" id="pills-collection" role="tabpanel"
+             aria-labelledby="pills-collection-tab">
+            <livewire:search/>
+        </div>
+        <div class="overflow-x-hidden hidden" id="pills-stats" role="tabpanel"
+             aria-labelledby="pills-stats-tab">
+            <div class="d-flex">
+                <x-stats-comp :mostPlayed="$mostPlayed" :lastPlayed="$lastPlayed"></x-stats-comp>
+            </div>
+        </div>
+        <div class="hidden" id="pills-options" role="tabpanel"
+             aria-labelledby="pills-options-tab">
+            <div class=d-flex">
+                <div class="align-self-center text-center mt-1">
+                    <x-options></x-options>
                 </div>
             </div>
-            <div class="hidden" id="pills-collection" role="tabpanel"
-                 aria-labelledby="pills-collection-tab">
-                <livewire:search/>
-            </div>
-            <div class="overflow-x-hidden hidden" id="pills-stats" role="tabpanel"
-                 aria-labelledby="pills-stats-tab">
-                <div class="d-flex">
-                    <x-stats-comp :mostPlayed="$mostPlayed" :lastPlayed="$lastPlayed"></x-stats-comp>
+        </div>
+        <div class="@if(\App\Models\User::query()->count() >=1) hidden @endif" id="pills-register" role="tabpanel"
+             aria-labelledby="pills-register-tab">
+            <div class=d-flex">
+                <div class="align-self-center text-center mt-1">
+                    <x-register></x-register>
                 </div>
             </div>
-            <div class="hidden" id="pills-options" role="tabpanel"
-                 aria-labelledby="pills-options-tab">
-                <div class=d-flex">
-                    <div class="align-self-center text-center mt-1">
-                        <x-options></x-options>
-                    </div>
-                </div>
-            </div>
-            <div class="@if(\App\Models\User::query()->count() >=1) hidden @endif" id="pills-register" role="tabpanel"
-                 aria-labelledby="pills-register-tab">
-                <div class=d-flex">
-                    <div class="align-self-center text-center mt-1">
-                        <x-register></x-register>
-                    </div>
-                </div>
-            </div>
+        </div>
 
-        </div>
-    @else
-        <div class="text-center hidden">
-            <a href="{{route('register')}}">
-                <button class="btn btn-primary">Register Your Account</button>
-            </a>
-        </div>
-    @endif
+    </div>
     <x-controls :nowPlaying="$nowPlaying"></x-controls>
     <x-modal></x-modal>
     <x-edit-modal></x-edit-modal>
