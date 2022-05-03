407e78bd6ab79d8eb32f963987d1e4ee016b6ada
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-05-03 15:13:32 -0400

    add error logging to genre chart

--- app/Charts/GenreChart.php
+++ app/Charts/GenreChart.php
@@ -9,6 +9,7 @@ use Chartisan\PHP\Chartisan;
 use ConsoleTVs\Charts\BaseChart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class GenreChart extends BaseChart
 {
@@ -19,16 +20,20 @@ class GenreChart extends BaseChart
      */
     public function handler(Request $request): Chartisan
     {
-        $mostPlayedGenres = Plays::query()
-            ->join('genres', 'plays.genre_id', '=', 'genres.id')
-            ->select('genres.name', DB::raw("count(*) as times_played"))
-            ->groupBy('plays.genre_id')
-            ->orderBy('times_played', 'DESC')
-            ->get();
-        $labels = $mostPlayedGenres->pluck('name');
-        $count = $mostPlayedGenres->pluck('times_played');
-        return Chartisan::build()
-            ->labels($labels->toArray())
-            ->dataset('Times Chosen', $count->toArray());
+        try {
+            $mostPlayedGenres = Plays::query()
+                ->join('genres', 'plays.genre_id', '=', 'genres.id')
+                ->select('genres.name', DB::raw("count(*) as times_played"))
+                ->groupBy('plays.genre_id')
+                ->orderBy('times_played', 'DESC')
+                ->get();
+            $labels = $mostPlayedGenres->pluck('name');
+            $count = $mostPlayedGenres->pluck('times_played');
+            return Chartisan::build()
+                ->labels($labels->toArray())
+                ->dataset('Times Chosen', $count->toArray());
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
     }
 }
