f4ee0064f31f7d1ec5e3b3a3cc32a9e78b2b9bb2
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-05-04 09:24:50 -0400

    fix css on wifi modal when in raspi mode

--- resources/views/components/wifi.blade.php
+++ resources/views/components/wifi.blade.php
@@ -1,7 +1,7 @@
 <div class="absolute hidden top-0 left-0 bg-gray-500/50 h-full w-full flex" id="wifiModal" tabindex="-1" role="dialog"
      aria-labelledby="exampleModalCenterTitle"
      aria-hidden="true">
-    <div class="mx-auto my-auto bg-white w-1/4" role="document">
+    <div class="mx-auto my-auto bg-white raspi:w-2/4 w-1/4" role="document">
         <div class="h-2/4 p-2 shadow-lg">
             <div class="relative border-b border-black">
                 <h5 class="text-lg" id="modal-title">Connect To Wifi</h5>
