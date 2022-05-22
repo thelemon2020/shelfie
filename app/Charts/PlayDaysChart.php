1caf2fb6515306ae40c53c86c82f9a3898387d98
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-05-22 10:43:30 -0400

    fix a bug with daysplayed chart not taking timezones into account

--- app/Charts/PlayDaysChart.php
+++ app/Charts/PlayDaysChart.php
@@ -12,13 +12,15 @@ use Illuminate\Http\Request;
 
 class PlayDaysChart extends BaseChart
 {
-
     public function handler(Request $request): Chartisan
     {
-        $playDays = collect(Plays::query()->get('created_at'))->groupBy(function ($date) {
-            return Carbon::parse($date->created_at)->format('l');
-        })->toArray();
-
+        $playDays = collect(Plays::query()->get('created_at'))
+            ->each(function ($date) {
+                $date->created_at = $date->created_at->setTimeZone('America/Toronto');
+            })
+            ->groupBy(function ($date) {
+                return Carbon::parse($date->created_at)->subHours(4)->format('l');
+            })->toArray();
         $daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         $releasesPerDay = array_map(function ($day) use ($playDays) {
             $numberOfPlays = $playDays[$day] ?? [];
