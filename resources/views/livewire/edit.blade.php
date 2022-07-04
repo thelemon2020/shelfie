d3e538bc2250c41711a8deb3755a0783e6460449
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2022-07-04 16:02:48 -0400

    wip

--- resources/views/livewire/edit.blade.php
+++ resources/views/livewire/edit.blade.php
@@ -3,9 +3,12 @@
         window.addEventListener('add-genre', event => {
             $('#genreModal').toggleClass('hidden')
         })
+        window.addEventListener('add-subgenre', event => {
+            console.log('here')
+            $('#subgenreModal').toggleClass('hidden')
+        })
         window.addEventListener('open-image-modal', event => {
             const elementsToRemove = $('#image-content').children('img');
-            console.log('here', elementsToRemove)
             for (let i = 0; i < elementsToRemove.length; i++) {
                 elementsToRemove[i].remove()
             }
@@ -22,31 +25,19 @@
                  wire:click="loadImages">
                 Change Cover Image
             </btn>
-            @error('release.full_image')<span class="error">{{ $message }}</span> @enderror
         </div>
         <div class="w-2/4 raspi:flex">
             <div class="raspi:pr-2">
                 <label class="block" for="artist">Artist</label>
                 <input type="text" name="artist" id="artistEdit" wire:model="release.artist">
-                @error('release.artist') <span class="error">{{ $message }}</span> @enderror
-
-
                 <label class="block" for="title">Title</label>
                 <input type="text" name="title" id="titleEdit" wire:model="release.title">
-                @error('release.title') <span class="error">{{ $message }}</span> @enderror
-
                 <label class="block" for="release_year">Release Year</label>
                 <input type="number" name="release_year" id="releaseYearEdit"
                        wire:model="release.release_year">
-                @error('release.release_year') <span class="error">{{ $message }}</span> @enderror
             </div>
-            <div class="raspi:pl-2">
-                <label class="block" for="shelf_order">Shelf Position</label>
-                <input type="number" id="shelfOrderEdit" min="1"
-                       wire:model="release.shelf_order">
-                @error('release.shelf_order') <span class="error">{{ $message }}</span> @enderror
-
-                <label class="block" for="genre">Genre</label>
+            <div class="raspi:pl-2 pt-4">
+                <label class="block" for="genre">Primary Genres</label>
                 <select wire:model="genre" class="overflow-hidden">
                     <option selected="selected">Choose a Genre...</option>
                     @foreach($allGenres as $genreItem)
@@ -55,15 +46,34 @@
                     @endforeach
                     <option value="add-modal">Add Genre...</option>
                 </select>
-                @error('release.genre_id') <span class="error">{{ $message }}</span> @enderror
                 @if($genres)
-                    @foreach($genres as $genre)
-                        <button type="button" :wire:key="'genre-'{{$genre->id}}"
-                                wire:click="removeGenre({{$genre->id}})">
-                            {{$genre->name}}
-                        </button>
-                        <span> </span>
+                    <div class="flex flex-row">
+                        @foreach($genres as $genre)
+                            <span class="text-red-700 cursor-pointer" :wire:key="'genre-'{{$genre->id}}"
+                                  wire:click="removeGenre({{$genre->id}})">
+                            {{$genre->name}}&nbsp;
+                        </span>
+                        @endforeach
+                    </div>
+                @endif
+                <label class="block" for="subgenre">Secondary Genres</label>
+                <select wire:model="subgenre" class="overflow-hidden">
+                    <option selected="selected">Choose a Subgenre...</option>
+                    @foreach($allSubgenres as $subgenreItem)
+                        <option @if(!$subgenres?->where('id',$subgenreItem->id)->isEmpty()) disabled
+                                @endif value="{{$loop->index}}">{{$subgenreItem->name}}</option>
                     @endforeach
+                    <option value="add-modal">Add Subgenre...</option>
+                </select>
+                @if($subgenres)
+                    <div class="flex flex-row">
+                        @foreach($subgenres as $subgenre)
+                            <span class="text-red-700 cursor-pointer" :wire:key="'subgenre-'{{$subgenre->id}}"
+                                  wire:click="removeSubgenre({{$subgenre->id}})">
+                                {{$subgenre->name}}&nbsp;
+                            </span>
+                        @endforeach
+                    </div>
                 @endif
             </div>
         </div>
@@ -83,5 +93,6 @@
     </form>
     <x-genre-modal></x-genre-modal>
     <x-image-modal></x-image-modal>
+    <x-subgenre-modal></x-subgenre-modal>
 </div>
 
