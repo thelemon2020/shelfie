e1730f158b8b8dba4936f9e5a1f34ff30a7bfa8f
Vehikl Team <sourceday@vehikl.com>
2022-07-11 16:48:48 -0400

    Setup typescript and render basic component

---
+++ resources/ts/app.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import * as ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById('home-root'));
+root.render(<h1>Hello, world!</h1>);
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
         laravel([
             'resources/css/app.css',
             'resources/js/app.js',
+            'resources/ts/app.tsx'
         ]),
         {
             name: 'blade',
