627677663a64b280cf372a149964cd3e33f7ed51
Vehikl Team <sourceday@vehikl.com>
2022-07-11 16:58:13 -0400

    install mantine component library

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import * as ReactDOM from "react-dom/client";
+import { AppShell, Navbar, Header } from '@mantine/core';
 
 const root = ReactDOM.createRoot(document.getElementById('home-root'));
-root.render(<h1>Hello, world!</h1>);
+root.render(
+    <AppShell
+        padding="md"
+        navbar={<Navbar width={{ base: 300 }} height={500} p="xs">{/* Navbar content */}</Navbar>}
+        header={<Header height={60} p="xs">{/* Header content */}</Header>}
+        styles={(theme) => ({
+            main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
+        })}
+    >
+        Hey YOU!
+    </AppShell>
+);
